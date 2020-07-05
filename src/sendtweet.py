956b038e7fb14582a568b1fb1b475338b2483fa6
Robert Kump <kumprj@miamioh.edu>
2020-07-05 18:55:55 -0400

    mvp

---
+++ src/sendtweet.py
@@ -0,0 +1,163 @@
+from twython import Twython
+import requests
+import random
+import psycopg2
+import datetime
+from dateutil.relativedelta import relativedelta
+from settings import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret,
+    td_key,
+    database_user,
+    database_password,
+    database_host,
+    database_port,
+    database_db,
+    share_count
+)
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+apiUrl = f"https://api.tdameritrade.com/v1/marketdata/AMZN/quotes?apikey={td_key}"
+apiUrl2 = f"https://api.tdameritrade.com/v1/marketdata/AMZN/pricehistory?apikey={td_key}&periodType=month&period=1&frequencyType=daily&frequency=1&needExtendedHoursData=false"
+
+def rds_connect():
+    return psycopg2.connect(user = database_user,
+                            password = database_password,
+                            host = database_host,
+                            port = database_port,
+                            database = database_db)
+
+
+# connection = rds_connect()
+# cursor = connection.cursor()
+# select_query = f'select endtime from public.customcandle_lasttime where type = \'{type_of_candle}\' and etf = \'{etf}\''
+# cursor.execute(select_query)
+# stored_time = cursor.fetchone() # Fetch the end time value from our table if it exists. Point of restart for the script. Only ever one element here.
+# cursor.close()
+# connection.close()
+
+def main():
+    # message = "Hello world!"
+    # twitter.update_status(status=message)
+    # print("Tweeted: %s" % message)
+
+    resp = requests.get(apiUrl)
+    amzn_json = resp.json()
+    closing_price = amzn_json['AMZN']['closePrice']
+    net_worth = int(closing_price * int(share_count))
+    net_worth_str = "{:,}".format(net_worth)
+    # print("$" + net_worth)
+
+    prev_day_resp = requests.get(apiUrl2)
+    prev_day_json = prev_day_resp.json()
+    prev_day_close = prev_day_json['candles'][-2]['close']
+    prev_worth = int(prev_day_close * int(share_count))
+    prev_worth_str = "{:,}".format(prev_worth)
+
+    net_change = abs(prev_worth - net_worth)
+    net_change_str = "{:,}".format(net_change)
+
+    up_down = 'down' if (prev_day_close > closing_price) else 'up'
+    gain_loss = 'loss' if (prev_day_close > closing_price) else 'gain'
+
+    #print(f"Today Jeff's $AMZN shares are worth ${net_worth_str} billion, {up_down} from ${prev_worth_str} billion yesterday. This is a change of ${net_change_str} and a {gain_loss} of {bowls_str} chicken burritos.")
+    recently_used = True
+    while recently_used:
+        tweet_text_from_db, item_cost, last_use, num_id, str_id = select_tweet()
+        today = datetime.datetime.now().date()
+        three_months = datetime.timedelta(3*365/12)
+        three_months_ago = today - three_months
+        print(three_months_ago)
+        print(last_use)
+        if three_months_ago < last_use:
+            continue
+        else:
+            recently_used = False
+            break
+
+    # item_cost = db_results[1]
+    bowls = int(net_change / item_cost)
+    bowls_str =  "{:,}".format(bowls)
+    tweet_text = f"Today Jeff's $AMZN shares are worth ${net_worth_str} billion, {up_down} from ${prev_worth_str} billion yesterday. This is a change of ${net_change_str} and a {gain_loss} of {bowls_str} {tweet_text_from_db}."
+    print(tweet_text)
+    update_db_date(num_id, str_id, last_use)
+    # pass
+
+
+def select_tweet():
+    connection = rds_connect()
+    cursor = connection.cursor()
+    # id = random.randint(1,5)
+    id = 0
+    select_query = f'select tweettext, item_cost, last_use, num_id, str_id from public.bezostweets where num_id = {id}'
+    cursor.execute(select_query)
+    db_results = cursor.fetchone() # Fetch the end time value from our table if it exists. Point of restart for the script. Only ever one element here.
+    cursor.close()
+    connection.close()
+    return db_results[0], db_results[1], db_results[2], db_results[3], db_results[4]
+
+
+def update_db_date(num_id, str_id, last_use):
+    connection = rds_connect()
+    cursor = connection.cursor()
+    today = datetime.datetime.now().date()
+    insert_query = f'''insert into public.bezostweets (num_id, str_id, last_use) values ({num_id}, '{str_id}', '{last_use}')
+                        ON CONFLICT (num_id, str_id) DO UPDATE SET last_use = '{today}';'''
+   
+    cursor.execute(insert_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+if __name__ == "__main__":
+    main()
+
+
+
+        # connection = psycopg2.connect(user = database_user,
+        #                         password = database_password,
+        #                         host = database_host,
+        #                         port = database_port,
+        #                         database = database_db)
+
+        # current_test_data = (currentTestSymbol, currentTestIndicator, currentTestLookbackPeriod, currentTestObservationPeriod, currentTestIndexFilter,
+        #                 currentTestIndicatorSmoothing, currentTestStartdate, currentTestEndDate, currentTestOverlapping, currentTestIndexFilterSwap, currentTestIndicatorLevel,
+        #                 currentTestMarketEnvironment, currentTestIndexMASlope, currentTotalReturn, currentAvgReturn, currentZSCore, currentBuyHoldReturn, currentWinRatePercent,
+        #                 currentAvgWin, currentAvgLoss, currentMaxRisk, currentTotalTrades, currentTotalPositive, currentTotalNegative, 
+        #                 currentTimeInMarket, todaysDate, currentTotalReturn, currentAvgReturn, currentZSCore, currentBuyHoldReturn, currentWinRatePercent, currentAvgWin,
+        #                 currentAvgLoss, currentMaxRisk, currentTotalTrades, currentTotalPositive, currentTotalNegative, currentTimeInMarket, currentMedReturn, currentStdDev)
+        # sql_insert = '''insert into "backtest_results_10ma" (symbol, indicator, lookback_period, observation_period, index_filter, indicator_smoothing, 
+        #                 test_start_date, test_end_date, exclude_overlapping, index_filter_swap, indicator_level, market_environment, 
+        #                 index_ma_slope, total_return, avg_return, z_score, buy_hold_return, win_rate, avg_win,
+        #                 avg_loss, max_risk, total_trades, total_pos, total_neg, time_in_mkt, todays_date)                    
+        #                 values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
+        #                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        #                 ON CONFLICT (indicator, observation_period, todays_date)
+        #                 DO UPDATE SET total_return = %s,
+        #                 avg_return = %s,
+        #                 z_score = %s,
+        #                 buy_hold_return = %s,
+        #                 win_rate = %s,
+        #                 avg_win = %s,
+        #                 avg_loss = %s,
+        #                 max_risk = %s,
+        #                 total_trades = %s,
+        #                 total_pos = %s,
+        #                 total_neg = %s,
+        #                 time_in_mkt = %s,
+        #                 median_return = %s,
+        #                 std_dev = %s;
+        #                 '''
+        # cursor = connection.cursor()
+        # cursor.execute(sql_insert, current_test_data)
+        # connection.commit()
+        # cursor.close()
+        # connection.close()
+        # print('Backtest Table updated for %s for period %s' % (currentTestIndicator, currentTestObservationPeriod))
